31ac85074a214c62777fd8acee6ca7a23fe11ae4
Raphael Albuquerque <raphael333af@gmail.com>
2025-11-25 10:07:28 -0300

    hamburguer adcionado

---
+++ menu.js
@@ -0,0 +1,10 @@
+function clickMenu(){
+    if(menu_paginas.style.display=='block'){
+        menu_paginas.style.display='none';
+    }
+    else{
+        menu_paginas.style.display='block';
+        
+    }
+
+}
