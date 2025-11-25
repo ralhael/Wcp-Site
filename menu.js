af20cdae02bd6cff098c1ffa1a345fa70bac6b44
Raphael Albuquerque <raphael333af@gmail.com>
2025-11-25 10:24:51 -0300

    remove logo after onclick

--- menu.js
+++ menu.js
@@ -1,9 +1,12 @@
 function clickMenu(){
-    if(menu_paginas.style.display=='block'){
+    if(menu_paginas.style.display=='grid'){
         menu_paginas.style.display='none';
+        wcpsemfundo.style.display='grid';
+        
     }
     else{
-        menu_paginas.style.display='block';
+        menu_paginas.style.display='grid';
+        wcpsemfundo.style.display='none'
         
     }
 
