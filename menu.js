94821defa299e9212e92f0e01949dbb7cb85a2d2
Raphael Albuquerque <raphael333af@gmail.com>
2025-11-25 10:46:28 -0300

    topo height is same after hamburguer now

--- menu.js
+++ menu.js
@@ -2,11 +2,14 @@ function clickMenu(){
     if(menu_paginas.style.display=='grid'){
         menu_paginas.style.display='none';
         wcpsemfundo.style.display='grid';
+        id_topo.style.margin='auto'
         
     }
     else{
         menu_paginas.style.display='grid';
-        wcpsemfundo.style.display='none'
+        wcpsemfundo.style.display='none';
+        id_topo.style.margin='1.3rem 0rem';
+       
         
     }
 
